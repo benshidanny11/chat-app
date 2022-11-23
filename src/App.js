f8752f49300d1212cce30f3b9736fc5d6ca615a0
benshidanny11 <benshidanny11@gmail.com>
2022-11-23 23:39:51 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // const [contacts] = useState([{text:'John Smith', active:false},{text:'Molly Watt', active:true},{text:'Ivan Mackay', active:false}]);
+  const [messages,setMessages]= useState( [{id:'1',text:'Hi Christine!', me:true},{id:'2',text:'Hey, how are you doing?', me:false},{id:'3',text:'It\'s been a while', me:false},{id:'4',text:'Yes it is!', me:true},{id:'5',text:'I have something to tell you', me:true},{id:'6',text:'Okay, what is it?', me:false},{id:'7',text:'I\'d like to tell you that I love you so much ❤️💓', me:true}],)
+  const [message, setMessage]= useState('');
+
+
+  
+  
+ const  handleChange=(e) => {
+    setMessage(e.target.value); 
+  }
+
+  const handleSubmit=(e) =>{
+    e.preventDefault();
+    if (!message.length) {
+      return;
+    }
+  
+    const newItem = {
+      text: message,
+      id: Date.now(),
+      me: true
+    };
+
+    
+
+    messages.push(newItem);
+    setMessages([...messages]);
+    setMessage('');
+  }
+
+
+let MessagesHistory = ({items})=> {
+
+    return [].concat(items).reverse().map(item => (
+        <div className={"message " + (item.me ? "me" : "")} key={item.id}>
+          <div className="message-body">
+            {item.text}
+          </div>
+        </div>
+      ));
+  }
+                 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="messages">
+        <div className="messages-history">
+          <MessagesHistory items={messages} />
+        </div>
+        <form className="messages-inputs" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Send a message" onChange={handleChange} value={message}/>
+            <button><i className="material-icons">send</i></button>
+        </form>
+      </div>
     </div>
   );
 }
